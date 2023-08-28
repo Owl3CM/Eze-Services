a29488ccbcbdf4ba325c470b7c04fce64822489c
Haider959 <saochiha@gmail.com>
2023-08-28 13:29:15 +0300

    v1.0.71

--- src/lib/Ui/Wrapper/Wrapper.tsx
+++ src/lib/Ui/Wrapper/Wrapper.tsx
@@ -37,11 +37,12 @@ class Wrapper extends React.Component {
   }
   render() {
     const { service, children, reloader, addStateBuilder, reload, loadMore, ...props } = this.props as IWrapperProps;
+    const { Component, containerClassName } = this.reloaderProps;
     return (
       <div id={this.id} {...props}>
         {reload && (
-          <div className={this.reloaderProps.containerClassName} style={{ display: "none" }} id="reloader-container">
-            {reloader}
+          <div className={containerClassName} style={{ display: "none" }} id="reloader-container">
+            {Component}
           </div>
         )}
         {children}
@@ -123,11 +124,10 @@ export default ({
   reload = service.reload,
   loadMore = service.loadMore,
   addStateBuilder = !!service.setState,
-  reloader = _reloader,
   className = "local-wrapper",
   ...props
 }: IWrapperProps) => {
-  const _props = { service, reload, loadMore, reloader, addStateBuilder, className, ...props };
+  const _props = { service, reload, loadMore, addStateBuilder, className, ...props };
   return <Wrapper {..._props} />;
 };
 
@@ -145,10 +145,9 @@ const _reloaderProps = {
     reloader.style.transform = `rotate(${rotateAngle}deg)`;
   },
   containerClassName: "",
+  Component: (
+    <svg className="reloader-svg" viewBox="0 0 512 512">
+      <path d="M256,431.967c-97.03,0-175.967-78.939-175.967-175.967c0-28.668,7.013-56.655,20.156-81.677l-25.144-16.639l107.282-54.228l-7.974,119.943l-26.111-17.279c-7.203,15.517-11.041,32.51-11.041,49.879c0,65.507,53.294,118.801,118.801,118.801s118.801-53.294,118.801-118.801s-53.295-118.8-118.802-118.8V80.033c97.028,0,175.967,78.938,175.967,175.967S353.028,431.967,256,431.967z" />
+    </svg>
+  ),
 };
-
-const _reloader = (
-  <svg className="reloader-svg" viewBox="0 0 512 512">
-    <path d="M256,431.967c-97.03,0-175.967-78.939-175.967-175.967c0-28.668,7.013-56.655,20.156-81.677l-25.144-16.639l107.282-54.228l-7.974,119.943l-26.111-17.279c-7.203,15.517-11.041,32.51-11.041,49.879c0,65.507,53.294,118.801,118.801,118.801s118.801-53.294,118.801-118.801s-53.295-118.8-118.802-118.8V80.033c97.028,0,175.967,78.938,175.967,175.967S353.028,431.967,256,431.967z" />
-  </svg>
-);
--- src/test/TestGrid.tsx
+++ src/test/TestGrid.tsx
@@ -13,6 +13,10 @@ const service = {
     this.template = template;
   },
   template: "One",
+  reload: async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log("reload");
+  },
 };
 
 const One = ({ item }: any) => (
@@ -29,7 +33,7 @@ const Two = ({ item }: any) => (
 
 const TestGrid = () => {
   return (
-    <Wrapper service={service}>
+    <Wrapper service={service} reloaderProps={reloaderProps}>
       <h1>TestGrid</h1>
 
       <div className="row gap-x p-l">
@@ -55,3 +59,29 @@ const TestGrid = () => {
 };
 
 export default TestGrid;
+
+const reloaderProps = {
+  reloadingClass: "squiggle",
+  onPull: ({ diff, diffPercentage, reloader }) => {
+    console.log(diff, diffPercentage, reloader);
+
+    const dashOffset = 650 + diffPercentage * 650;
+    (reloader.firstChild.lastChild as any).style.strokeDashoffset = `${dashOffset}`;
+  },
+  Component: (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-50 -50 500 500">
+      <path
+        className="reload-squiggle-back"
+        fill="none"
+        stroke="#DA374633"
+        strokeWidth="47"
+        d="M111.6,344.3h217.2c33.8-2.5,61.2-29.9,61.2-63.7V88.8c0-33.9-27.5-61.4-61.4-61.4H87.3c-33.8,0-61.1,27.4-61.1,61.1l0.6,238.7c0.1,34.6,28.3,62.5,63.1,62.5h67.8"></path>
+      <path
+        fill="none"
+        stroke="#DA3746"
+        strokeWidth="46"
+        className="reload-squiggle"
+        d="M111.6,344.3h217.2c33.8-2.5,61.2-29.9,61.2-63.7V88.8c0-33.9-27.5-61.4-61.4-61.4H87.3c-33.8,0-61.1,27.4-61.1,61.1l0.6,238.7c0.1,34.6,28.3,62.5,63.1,62.5h67.8"></path>
+    </svg>
+  ),
+};
